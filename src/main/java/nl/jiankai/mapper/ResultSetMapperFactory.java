19c3c865f9352c52bd785db158d168338a0dad79
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-12-06 02:32:44 +0100

    refactor(factory): made constructor private and throw error if called

--- src/main/java/nl/jiankai/mapper/ResultSetMapperFactory.java
+++ src/main/java/nl/jiankai/mapper/ResultSetMapperFactory.java
@@ -9,6 +9,10 @@ import org.slf4j.LoggerFactory;
 public class ResultSetMapperFactory {
     private final Logger logger = LoggerFactory.getLogger(ResultSetMapperFactory.class);
 
+    private ResultSetMapperFactory() {
+        throw new AssertionError("The ResultSetMapperFactory should not be instantiated.");
+    }
+
     public static ResultSetMapper getResultSetMapperLowerCaseUnderscore() {
         return getResultSetMapper(new LowerUnderscoreFieldNamingStrategy());
     }
