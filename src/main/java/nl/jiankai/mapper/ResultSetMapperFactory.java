0cb3e185f6b422ab640123f08b7745b2602a8337
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-12-06 15:44:00 +0100

    add(factory): ResultSetMapper with LowerCaseDashes

--- src/main/java/nl/jiankai/mapper/ResultSetMapperFactory.java
+++ src/main/java/nl/jiankai/mapper/ResultSetMapperFactory.java
@@ -2,6 +2,7 @@ package nl.jiankai.mapper;
 
 import nl.jiankai.mapper.strategies.FieldNamingStrategy;
 import nl.jiankai.mapper.strategies.IdentityFieldNamingStrategy;
+import nl.jiankai.mapper.strategies.LowerCaseDashesFieldNamingStrategy;
 import nl.jiankai.mapper.strategies.LowerCaseUnderscoreFieldNamingStrategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +20,10 @@ public class ResultSetMapperFactory {
 
     public static ResultSetMapper getResultSetMapperIdentity() {
         return getResultSetMapper(new IdentityFieldNamingStrategy());
+    }
+
+    public static ResultSetMapper getResultSetMapperLowerCaseDashes() {
+        return getResultSetMapper(new LowerCaseDashesFieldNamingStrategy());
     }
 
     private static ResultSetMapper getResultSetMapper(FieldNamingStrategy fieldNamingStrategy) {
