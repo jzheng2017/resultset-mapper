228538067a205db40bb55c43d9d194959d700ba6
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-12-16 23:54:19 +0100

    docs(annotations): add java doc to Column annotation

--- src/main/java/nl/jiankai/annotations/Column.java
+++ src/main/java/nl/jiankai/annotations/Column.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation can be used to override the {@link nl.jiankai.mapper.strategies.FieldNamingStrategy}.
+ */
 @Target(value = {ElementType.FIELD})
 @Retention(value = RetentionPolicy.RUNTIME)
 public @interface Column {
