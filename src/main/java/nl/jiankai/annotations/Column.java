6f4c636da23acd174724c4802c592b03bd8abdcf
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-12-16 22:56:18 +0100

    fix(annotations): remove bug annotation applicable to method

--- src/main/java/nl/jiankai/annotations/Column.java
+++ src/main/java/nl/jiankai/annotations/Column.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(value = {ElementType.METHOD, ElementType.FIELD})
+@Target(value = {ElementType.FIELD})
 @Retention(value = RetentionPolicy.RUNTIME)
 public @interface Column {
     String name();
