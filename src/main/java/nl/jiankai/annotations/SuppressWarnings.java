da45a6847613919adb8c6b8a1a0017a962af98f8
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-12-20 12:52:08 +0100

    docs: improve javadoc

--- src/main/java/nl/jiankai/annotations/SuppressWarnings.java
+++ src/main/java/nl/jiankai/annotations/SuppressWarnings.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Target;
 /**
  * This annotation can be used to suppress logging warnings when retrieving value out of the {@link java.sql.ResultSet} has failed for some reason.
  * It can be used at class and field level. By using it on class level all warnings for all fields in that class will be suppressed.
- * By using it on field level only the individual logging warnings will be suppressed.
+ * By using it on field level only the individual logging warnings for the annotated field will be suppressed.
  *
  * @author Jiankai Zheng (jk.zheng@hotmail.com)
  * @since 1.1.0
