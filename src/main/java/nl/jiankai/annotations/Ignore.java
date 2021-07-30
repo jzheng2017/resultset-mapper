f54b28d0404bd5f00679dc2363b971979c14f35d
Jiankai Zheng <J.Zheng1@student.han.nl>
2021-07-30 12:33:36 +0200

    docs: fix javadoc missing bracket

--- src/main/java/nl/jiankai/annotations/Ignore.java
+++ src/main/java/nl/jiankai/annotations/Ignore.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
  * This annotation can be used on a field to let the {@link nl.jiankai.mapper.ResultSetMapper} know that it can skip this field when mapping.
  * This means that the {@link nl.jiankai.mapper.ResultSetMapper} will not try to retrieve the value from the {@link java.sql.ResultSet} for the annotated field.
  *
- * @author Jiankai Zheng (jk.zheng@hotmail.com
+ * @author Jiankai Zheng (jk.zheng@hotmail.com)
  * @since 1.2.0
  */
 @Target(value = {ElementType.FIELD})
